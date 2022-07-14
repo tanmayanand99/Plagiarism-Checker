30d47c55c589bd279b77f93567a199a91b0f96d3
Tanmay Anand <tanmayanand99@gmail.com>
2022-07-14 18:20:58 +0530

    Added

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask import render_template,request
+from nltk.tokenize import sent_tokenize
+import numpy as np
+from preprocessing import removeUnnecessary,removeStopWords,porterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+import nltk
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+nltk.download('stopwords')
+
+p = np.load('data.npy')
+notprocessed = np.load('notprocessed.npy')
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def check():
+    data = request.form['text']
+    sent=sent_tokenize(data)
+    npt = np.append(notprocessed,sent)
+    for i in range(len(sent)):
+        sent[i]=" ".join(porterStemmer(removeStopWords(removeUnnecessary(sent[i]).split(" "))))
+    flen = len(p)
+    k=np.append(p,sent)
+    my_stop_words = stopwords.words('english')
+    tf_idf_vect = TfidfVectorizer(stop_words=my_stop_words)
+    X_train_tf_idf = tf_idf_vect.fit_transform(k)
+    terms = tf_idf_vect.get_feature_names()
+    final={}
+    sum=0
+    for i in range(flen,len(k)):
+        dp = cosine_similarity(X_train_tf_idf[i],X_train_tf_idf[0:flen])
+        m=np.argmax(dp)
+        print("Simmilarity With:")
+        sum=sum+(dp[0][m]*100)
+        if(dp[0][m]>0.3):
+            final[npt[i]]=npt[m]
+            print(dp[0][m])
+        else:
+            final[npt[i]]="0"
+            print("No simmiliarity")
+        print()
+    sim=sum/(len(k)-flen)
+    return render_template('main.html', data=final,sim=sim)
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ preprocessing.py
@@ -0,0 +1,21 @@
+import re
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+
+stemmer=PorterStemmer()
+
+def removeUnnecessary(text):
+    text = text.lower()
+    text = re.sub("[^A-Za-z ]","",text)
+    return text
+def removeStopWords(text):
+    stop_words = set(stopwords.words('english'))
+    text1 = [word for word in text if word not in stop_words]
+    return text1
+def porterStemmer(text):
+    text1 = [stemmer.stem(word) for word in text]
+    return text1
+def applyMyFuncs(text):
+    return porterStemmer(removeStopWords(removeUnnecessary(text)))
+    
